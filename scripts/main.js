00466db4940beb3fa8c61938b1e0ba3691562fd4
Sergey Komarov <sergey.komarov.2004@mail.ru>
2023-06-24 22:48:14 +0300

    converted one step added

--- scripts/main.js
+++ scripts/main.js
@@ -1,8 +1,19 @@
-import iconArr from './iconArr.js';
+import iconArr from "./iconArr.js";
 
 const courseDate = document.getElementById("course__date");
 const courseTable = document.getElementById("course__table");
 const timeLoading = document.getElementById("time-loading");
+const converter = document.getElementById("convert");
+const converted = document.getElementById("converted");
+const converterConvertBox = document.getElementById("converter__convert-box");
+// const converterSelecter = document.querySelector(".converter__selecter");
+// const converterSelecterItems = document.querySelectorAll(
+//   ".converter__selecter-item"
+// );
+const converterConvertBoxtext = document.querySelector(
+  ".converter__convert-boxtext"
+);
+// const convertSelecterList = document.querySelector(".converter__selecter-list");
 
 fetch("https://www.cbr-xml-daily.ru/daily_json.js")
   .then((response) => response.json())
@@ -43,3 +54,74 @@ fetch("https://www.cbr-xml-daily.ru/daily_json.js")
 		  `;
     }
   });
+
+//   превратить селектор в массив и создать самому функция
+
+converter.addEventListener("input", (e) => {
+  converted.value = (e.target.value * converter.dataset.value).toFixed(2);
+});
+
+converterConvertBox.addEventListener("click", () => {
+  if (document.querySelectorAll(".converter__selecter").length === 1) {
+    document.querySelector(".converter__selecter").remove();
+  } else {
+    generateValuteList();
+  }
+});
+
+async function generateValuteList() {
+  const converterSelector = document.createElement("div");
+  const converterSelectorClose = document.createElement("button");
+  const converterSelectorList = document.createElement("ul");
+
+  converterSelector.classList.add("converter__selecter");
+  converterSelectorClose.classList.add("converter__selector-close");
+  converterSelectorList.classList.add("converter__selecter-list");
+
+  converterSelectorClose.innerHTML = `
+  <svg width="40" height="40" viewBox="0 0 40 40" fill="none" xmlns="http://www.w3.org/2000/svg">
+							<path d="M26 14L14 26" stroke="#777F85" stroke-linecap="round" stroke-linejoin="round" />
+							<path d="M14 14L26 26" stroke="#777F85" stroke-linecap="round" stroke-linejoin="round" />
+						</svg>`;
+
+  document.querySelector(".converter__content").append(converterSelector);
+  converterSelector.append(converterSelectorClose);
+  converterSelector.append(converterSelectorList);
+
+  converterSelectorClose.addEventListener("click", () => {
+    converterSelector.remove();
+	document.querySelector(".container").style.padding = "0 15px";
+  });
+
+  if (window.matchMedia("(max-width: 578px)").matches) {
+    document.body.style.overflow = "hidden";
+    document.querySelector(".container").style.padding = "0";
+  }
+
+  const response = await fetch("https://www.cbr-xml-daily.ru/daily_json.js");
+  const data = response.json();
+  data.then((json) => {
+    const valute = Object.entries(json.Valute).map((i) => i[1]);
+    for (let i = 0; i < valute.length; i++) {
+      converterSelectorList.innerHTML += `
+		  <li data-target="${valute[i].Value}" data-charcode="${valute[i].CharCode}" class="converter__selecter-item">
+		  <button class="converter__selecter-button">
+		  <span>${valute[i].Name}</span>
+		  <strong>${valute[i].CharCode}</strong>
+		  </button>
+		  </li> 
+		  `;
+    }
+    document.querySelectorAll(".converter__selecter-item").forEach((item) => {
+      item.addEventListener("click", (e) => {
+		document.querySelector(".container").style.padding = "0 15px";
+        converter.dataset.value = e.currentTarget.dataset.target;
+        converterConvertBoxtext.textContent = e.currentTarget.dataset.charcode;
+        converterSelector.remove();
+        converted.value = (converter.value * converter.dataset.value).toFixed(
+          2
+        );
+      });
+    });
+  });
+}
